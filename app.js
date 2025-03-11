15cc182b86dae5b5bfbdfd6606f719cf0475091a
liliird <A01711949@tec.mx>
2025-03-10 22:59:35 -0600

    Lab 5: Frameworks de estilo

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const app = express();
 
-const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
+/* const path = require('path');
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+ */
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "string secreto",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Middleware
+/* const usersRoutes = require('./routes/usersRoutes');
+app.use('/users', usersRoutes); */
+
 app.use((request, response, next) => {
   console.log("Middleware!");
 
@@ -16,16 +31,17 @@ app.use((request, response, next) => {
   next();
 });
 
-const plantasRoutes = require("./routes/plantas.routes");
-
-app.use("/plantas", plantasRoutes);
+app.get("/ivd", (request, response, next) => {
+  response.send("Ya entró");
+  next();
+});
 
 app.use((request, response, next) => {
   console.log("Otro middleware!");
 
   //Manda la respuesta
-  response.statusCode = 404;
-  response.send("No se encuentra el recurso que estás buscando");
+  //response.statusCode = 404;
+  response.send("¡Hola mundo!");
 });
 
 app.listen(3000);
